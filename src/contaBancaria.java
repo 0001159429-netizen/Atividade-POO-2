9950873595e46e6927708372e2666ba310579411
Leticia <0001159429@senaimgaluno.com.br>
2025-11-10 19:38:28 -0300

    Meu primeiro Commit

---
+++ src/contaBancaria.java
@@ -0,0 +1,20 @@
+public class contaBancaria {
+
+    private String titular;
+    private int numeroConta;
+    private double saldo;
+}
+
+//Construtor
+
+public contaBancaria( String titular ) {
+
+    this.titular = titular;
+    this.numeroConta = numeroConta;
+    this.saldo = 0;
+}
+
+
+
+
+
